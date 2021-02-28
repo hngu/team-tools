bbf70e2563308125933a964bbdc76ae24d70cb4a
Huy Ngu <huy.ngu@compass.com>
2021-02-28 18:53:09 -0500

    adding app

---
+++ src/apps/video-dl/src/index.tsx
@@ -0,0 +1,8 @@
+import React, { FC } from 'react';
+import { render } from 'react-dom';
+
+const Application: FC = () => (
+  <h1>Application</h1>
+);
+
+render(<Application />, document.getElementById('root'));
